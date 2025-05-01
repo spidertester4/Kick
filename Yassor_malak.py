ce68da1af42b9b7eadb09aa10d069643b2b06002
spidertester4 <spidertester4@gmail.com>
2025-05-01 18:18:00 +0300

    Create Yassor_malak.py

---
+++ Yassor_malak.py
@@ -0,0 +1,14 @@
+import time
+import requests
+
+url = "https://kick.com/api/v2/messages/send/16409618"
+headers = {
+    "Authorization": "Bearer 207575384|VM2TjP93pkyfOxxlZb9zeKt5eNRRHtj5qeooggnC",
+    "Content-Type": "application/json"
+}
+data = {"content":"[emote:39268:HYPERCLAPH]", "type":"message"}
+
+for i in range(240):  # 3 hours = 180 minutes
+    res = requests.post(url, json=data, headers=headers)
+    print(f"[{i+1}/180] Status: {res.status_code}")
+    time.sleep(60)
