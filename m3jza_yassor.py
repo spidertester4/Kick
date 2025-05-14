4665ef03c48769f78d0d2a867c819b760a4fb0f7
spidertester4 <spidertester4@gmail.com>
2025-05-14 16:51:48 +0300

    Update m3jza_yassor.py

--- m3jza_yassor.py
+++ m3jza_yassor.py
@@ -1,14 +1,26 @@
 import time
 import requests
+import random
 
 url = "https://kick.com/api/v2/messages/send/15720418"
 headers = {
     "Authorization": "Bearer 207575384|VM2TjP93pkyfOxxlZb9zeKt5eNRRHtj5qeooggnC",
     "Content-Type": "application/json"
 }
-data = {"content":"[emote:39268:HYPERCLAPH]", "type":"message"}
+
+emotes = [
+    "[emote:37244:modCheck]",
+    "[emote:39402:Flowie]",
+    "[emote:37242:catblobDan]",
+    "[emote:39258:coffinPls]",
+    "[emote:39268:HYPERCLAPH]",
+    "!points",
+    "!xp"
+]
 
 for i in range(180):  # 3 hours = 180 minutes
+    random_emote = random.choice(emotes)
+    data = {"content": random_emote, "type": "message"}
     res = requests.post(url, json=data, headers=headers)
-    print(f"[{i+1}/180] Status: {res.status_code}")
-    time.sleep(60)
+    print(f"[{i+1}/180] Sent: {random_emote} | Status: {res.status_code}")
+    time.sleep(120)
