264e3a39182d6bc5ce5a5e7d4e13fdf446024351
spidertester4 <spidertester4@gmail.com>
2025-05-16 17:04:40 +0300

    Create 1revo_victim.py

---
+++ 1revo_victim.py
@@ -0,0 +1,25 @@
+import time
+import requests
+import random
+
+url = "https://kick.com/api/v2/messages/send/33274416"
+headers = {
+    "Authorization": "Bearer 214176003|8e5NWfg8A3R4XCIdCASqd8Hc0HyTTGCxrNAerKSA",
+    "Content-Type": "application/json"
+}
+
+emotes = [
+    "[emote:37244:modCheck]",
+    "[emote:39402:Flowie]",
+    "[emote:37242:catblobDan]",
+    "[emote:39258:coffinPls]",
+    "[emote:39268:HYPERCLAPH]",
+    "!points"
+]
+
+for i in range(180):  # 3 hours = 180 minutes
+    random_emote = random.choice(emotes)
+    data = {"content": random_emote, "type": "message"}
+    res = requests.post(url, json=data, headers=headers)
+    print(f"[{i+1}/180] Sent: {random_emote} | Status: {res.status_code}")
+    time.sleep(120)
