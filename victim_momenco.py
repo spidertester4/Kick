c572f6f4ee03c0db23c00d0f3089aeb5b4a50247
spidertester4 <spidertester4@gmail.com>
2025-05-17 21:10:48 +0300

    Update and rename victim_k1krm.py to victim_momenco.py

--- victim_momenco.py
+++ victim_momenco.py
@@ -2,7 +2,7 @@ import time
 import requests
 import random
 
-url = "https://kick.com/api/v2/messages/send/14751731"
+url = "https://kick.com/api/v2/messages/send/40562527"
 headers = {
     "Authorization": "Bearer 214176003|8e5NWfg8A3R4XCIdCASqd8Hc0HyTTGCxrNAerKSA",
     "Content-Type": "application/json"
@@ -18,9 +18,9 @@ emotes = [
     "!xp"
 ]
 
-for i in range(180):  # 3 hours = 180 minutes
+for i in range(60):  # 3 hours = 180 minutes
     random_emote = random.choice(emotes)
     data = {"content": random_emote, "type": "message"}
     res = requests.post(url, json=data, headers=headers)
     print(f"[{i+1}/180] Sent: {random_emote} | Status: {res.status_code}")
-    time.sleep(120)
+    time.sleep(180)
